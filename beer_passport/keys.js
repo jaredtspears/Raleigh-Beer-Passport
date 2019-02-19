e88eb3c08d86a17e73b4f4b94efc60938ee3c10a
Ha1f_Deaf <jaredtspears@gmail.com>
2019-02-19 18:44:32 -0500

    I added dotenv npm package  so that I could add in a .env file for my breweryDB key and url for their sandbox trial of their API. Next set up a keys.js to pull from that .env file of which this .env file was added to the .gitignore for protection on github repo. Comments added for persons attempting to make a similar app within the visible files. Next added axios npm package for future dev needs

---
+++ beer_passport/keys.js
@@ -0,0 +1,7 @@
+//breweryDB export:
+console.log('BreweryDB: this is loaded.');
+
+// #getYourOwnKeyTheirSite
+exports.breweryDB = {
+  key: process.env.SANDBOX_KEY
+};
--- beer_passport/src/index.js
+++ beer_passport/src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 // for reactstrap
 import 'bootstrap/dist/css/bootstrap.css';
+// requireing the breweryDB Sandbox Key:
+require('dotenv').config();
 
+// const keys = require('keys.js');  //still working this out 
+
+//TODO the below might not be how to correctly call this: REF LIRI app with Spotify
+// const breweryDB = require(keys.breweryDB); //from the key.js file set up in the .gitIgnore you will pull you the constructor "breweryDB"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
